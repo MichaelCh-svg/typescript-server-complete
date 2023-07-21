804700fdf9a7d7d9c59683374f96704e387483c2
MichaelCh-svg <champeniosmichael@gmail.com>
2023-07-21 05:39:11 -0600

    docs added for creating local test.

---
+++ src/local/test-example.ts
@@ -0,0 +1,5 @@
+/**
+ Create a 'test.ts' file in the 'local' directory to test the code without saving the test to github.
+ This file is ignored by github because the .gitignore file lists 'src/local/test.ts'.
+ 
+ */
