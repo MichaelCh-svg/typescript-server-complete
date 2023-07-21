799407d383ce5d3aaa1958eb74bc79f4f1908e8a
MichaelCh-svg <champeniosmichael@gmail.com>
2023-07-21 05:40:49 -0600

    code copied from test.ts to test-example.ts

--- src/local/test-example.ts
+++ src/local/test-example.ts
@@ -1,5 +1,16 @@
 /**
- Create a 'test.ts' file in the 'local' directory to test the code without saving the test to github.
- This file is ignored by github because the .gitignore file lists 'src/local/test.ts'.
- 
- */
+This file is listed in the .gitignore file. You can edit it without it showing up as a non-committed change.
+
+Here is an example of a local test.
+To run the test, type 'ts-node src/local/test' in the terminal.
+Note that async and await are not supported in the file called by 'ts-node'.
+However, you can still use .then statements to handle the response.
+**/
+
+import { LoginRequest } from "tweeter-entities-chemps-svg";
+import { getUserService } from "../lambda/factory/factory";
+
+let userAlias = '@colonel';
+let password = 'password';
+let loginRequest = new LoginRequest(userAlias, password);
+getUserService().loginFromService(loginRequest).then(resp => console.log(resp));
